e42c8c0fd8d1248f077a29cffa393c3a7621ff96
pedrotanak <106190056+pedrotanak@users.noreply.github.com>
2022-12-08 16:32:59 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+function startClassification()
+{
+  navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/q1hNPNSmM/model.json", modelReady);
+}
+
+function modelReady(){
+  classifier.classify( gotResults);
+}
+
+function gotResults(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    random_number_r = Math.floor(Math.random() * 255) + 1;
+    random_number_g = Math.floor(Math.random() * 255) + 1;
+    random_number_b = Math.floor(Math.random() * 255) + 1;
+
+    document.getElementById("result_label").innerHTML = 'Posso ouvir - '+ results[0].label;
+    document.getElementById("result_confidence").innerHTML = 'Precisão - '+ (results[0].confidence*100).toFixed(2)+" %";
+    document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+    document.getElementById("result_confidence").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+
+ alien1=document.getElementById("alien1");
+ alien2=document.getElementById("alien2");
+ alien3=document.getElementById("alien3");
+ alien4=document.getElementById("alien4");
+if(results[0].label=="palmas"){
+  alien1.src="aliens-01.gif";
+  alien2.src="aliens-02.png";
+  alien3.src="aliens-03.png";
+  alien4.src="aliens-04.png";
+}
+else if(results[0].label=="teclado"){
+  alien1.src="aliens-01.png";
+  alien2.src="aliens-02.gif";
+  alien3.src="aliens-03.png";
+  alien4.src="aliens-04.png";
+}
+else if(results[0].label=="botao"){
+  alien1.src="aliens-01.png";
+  alien2.src="aliens-02.png";
+  alien3.src="aliens-03.gif";
+  alien4.src="aliens-04.png";
+}
+else{
+  alien1.src="aliens-01.png";
+  alien2.src="aliens-02.png";
+  alien3.src="aliens-03.png";
+  alien4.src="aliens-04.gif";
+}
+}
+}
